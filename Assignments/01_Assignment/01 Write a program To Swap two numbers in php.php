dc133e5ed616de956abea8373d21dc1405983a10
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2023-11-27 21:13:07 +0530

    Practice

---
+++ Assignments/01_Assignment/01 Write a program To Swap two numbers in php.php
@@ -0,0 +1,13 @@
+<?php
+    $No1 = 15;
+    $No2 = 25;
+    $Temp = 0;
+
+    echo "Before Swap Values <br> $No1 $No2 <br>";
+
+    $Temp = $No1;
+    $No1 = $No2;
+    $No2  = $Temp;
+
+    echo "After Swap Values <br> $No1 $No2";
+?>
